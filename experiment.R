4f17cf1e038480db8685d15791c885b622ccedf8
Mario Fajardo <mario.fajardopedraza@sydney.edu.au>
2015-01-30 15:22:25 +1100

    peak.R as Jose wrote it...and little bit more of experiment.R

--- experiment.R
+++ experiment.R
@@ -3,29 +3,57 @@ require(MASS)
 require(ggplot2)
 require(reshape2)
 require(Rwave)
+require(EBImage)
 FACSC20141216_files <- dir('.','.fcs',full.names = T)
 
-# findPeaks <- function(file,
-#                       peaks=6,
-#                       resol=500) {
-
-  resol=500
-  tolerance<-.995
-  peaks=5
-  data <- read.FCS(FACSC20141216_files[[1]], transformation=FALSE,alter.names = T)
+findPeaks1 <- function(file,tolerance=.995,peaks=5,plot=F) {
+  resol=250
+  data <- read.FCS(file, transformation=FALSE,alter.names = T)
   to_plot <- data.frame(x=data@exprs[,'PI.H'],y=data@exprs[,'DAPI.H'])
-  plot(log(to_plot$x),log(to_plot$y))
+#   plot(log(to_plot$x),log(to_plot$y))
   kern<-kde2d(log(to_plot$x),log(to_plot$y),n = resol)
   mask<-kern$z*as.numeric(kern$z>quantile(kern$z,tolerance))
-  to_plot<-data.frame(x=rep(kern$x,resol),y=rep(kern$y,each=resol),z=melt(kern$z)[,3])
+  
+  
+  to_plot_kern<-data.frame(x=rep(kern$x,resol),y=rep(kern$y,each=resol),z=melt(kern$z)[,3])
   
 #this doesn't make sense to me, it should be colMeans...but is not ... i need some sleep#
-  local_maximas_x<-kern$x[which(diff(sign(diff(rowMeans(kern$z*mask))))==-2)+1][1:peaks]
-   
-  ggplot(to_plot,aes(x,y))+
+#REMINDER: always read the help file!... 
+#kern2d creates a matrix were x are rows....now peace reigns again#
+  loc_max<-function(DATA) which(diff(sign(diff(DATA)))==-2)+1
+  loc_max_posit_x<-loc_max(rowSums(kern$z*mask))
+  local_maximas_x<-kern$x[loc_max_posit_x]
+  if(length(na.exclude(local_maximas_x))<peaks) {
+    warning(paste('only',length(na.exclude(local_maximas_x)),' peaks were higher that the tolerance value'))
+    local_maximas_x<-local_maximas_x[complete.cases(local_maximas_x)]
+    peaks<-length(local_maximas_x)
+  }
+  loc_max_posit_y<-sapply(loc_max_posit_x[order(local_maximas_x)[1:peaks]],function(x) tmp<-which.max(kern$z[x,]))
+  
+  local_maximas_y<-sapply(loc_max_posit_x[order(local_maximas_x)[1:peaks]],function(x) {
+    tmp<-which.max(kern$z[x,])
+    kern$y[tmp]
+  })
+  local_maximas_x<-kern$x[loc_max_posit_x][1:peaks]
+
+# ggplot(to_plot,aes(log(x),log(y)))+
+#   geom_point()+
+#   coord_equal()+
+#   labs(x='log(PI-H)',y='log(DAPI-H)')
+if(plot){
+    ggplot(to_plot_kern,aes(x,y))+
     geom_tile(aes(fill=z))+
     coord_equal()+
     labs(x='log(PI-H)',y='log(DAPI-H)')+
-    geom_vline(xintercept=local_maximas_x,linetype=2,aes(colour=local_maximas_x))
-  
-  
+    geom_vline(xintercept=local_maximas_x,linetype=2,colour=local_maximas_x)+
+    geom_hline(yintercept=local_maximas_y,linetype=2,colour=local_maximas_x)
+}
+# pix_size_x <- abs(kern$x[1]- kern$x[2])
+# pix_size_y <- abs(kern$y[1]- kern$y[2])
+# 
+# 
+# buffer <- mapply(FUN = function(x,y) kern$z[x-x*(buffer_size/pix_size_x):x-buffer_size/pix_size_x,y],loc_max_posit_x,loc_max_posit_y)
+
+}  
+
+findPeaks1(file = FACSC20141216_files[[3]],tolerance = .995,peaks = 5,plot = T)
--- peaks.R
+++ peaks.R
@@ -1,14 +1,13 @@
 library(raster)
 require(flowCore)
 require(maps)
-require(ggplot2)
 
 FACSC20141216_files <- dir('.','.fcs',full.names = T)
 
-findPeaks <- function(file,silent=F,pimp_my_plot=FALSE,peak_max=6) {
+findPeaks <- function(file,silent=F) {
   data <- read.FCS(file, transformation=FALSE,alter.names = T)
   to_plot <- data.frame(x=data@exprs[,'PI.H'],y=data@exprs[,'DAPI.H'])
-  to_plot <- data_ <- to_plot #in order to make colMeans in simplify function below
+  to_plot <- data_ <- log(to_plot) 
   
   coordinates(data_) <- ~ x + y
   
@@ -16,7 +15,7 @@ findPeaks <- function(file,silent=F,pimp_my_plot=FALSE,peak_max=6) {
   test <- rasterize(data_,template,data_@coords[,1],fun='count')
   
   test_ <- na.exclude(as.data.frame(cbind(xyFromCell(test,cell = 1:ncell(test)),test[])))
-  lines <- test_[test_[,3] > quantile(test_[,3],0.995,na.rm = T),1:3]
+  lines <- test_[test_[,3] > quantile(test_[,3],0.995,na.rm = T),1:2]
     
   simplify <- function(lines) {
     aaa <- lapply(1:nrow(lines), function(x) {
@@ -28,7 +27,6 @@ findPeaks <- function(file,silent=F,pimp_my_plot=FALSE,peak_max=6) {
     
     bbb <- lines[sapply(aaa,length) == 1,]
     
-    #the problem of this part is that the scale is in log scale so colMeans dos not applies properly  
     aaaa <- sapply(max(sapply(aaa,length)):2,function(x){
       step <- aaa[sapply(aaa,length) == x]
       if(length(step) == 0) return(NULL)
@@ -52,29 +50,12 @@ findPeaks <- function(file,silent=F,pimp_my_plot=FALSE,peak_max=6) {
   buffer <- extract(test,cc,buffer=0.25,fun=sum)
   values <- extract(test,cc,fun=mean)
   
-  cc<-cc[values,]
-  if((nrow(cc)< peak_max)) warning('Maximum number of peaks reached, reduce the threshold value in order to find more peaks')
   
-  
-  #   ggplot(test_,aes(x,y,fill=V3)) + geom_raster()  + xlab('log(PI-H)') + ylab('log(DAPI-H)') + geom_vline(aes(xintercept=x),lines,linetype='longdash',colour='red') + geom_hline(aes(yintercept=y),lines,linetype='longdash',colour='red')
-    if(pimp_my_plot){
-    names(test_)[3] <- 'density'
-    pimp<-ggplot(test_, aes(x=x, y=y)) + 
-    geom_tile(aes(fill = density)) + 
-    coord_equal()+
-    labs(x='log(PI-H)',y='log(DAPI-H)')+
-    geom_vline(xintercept=cc$x,linetype=2)+
-    geom_hline(yintercept=cc$y,linetype=2)+
-    geom_point(data = cc,aes(x,y),size=7.5)+
-    theme_bw()
-    print(pimp)
-    } else {
       plot(test,xlab='log(PI-H)',ylab='log(DAPI-H)')
       sapply(cc[,1],function(x) abline(v=x,lty=2))
       sapply(cc[,2],function(x) abline(h=x,lty=2))
       #apply(cc,1,function(x) points(x=x[1],y=x[2],col='blue',cex=2))
       symbols(cc,circles=rep(0.25,nrow(cc)),inches=F,add=T)
-    }
   #cat(c("Peaks' coordinates:",apply(as.matrix(cc),1,function(x) paste0(round(x,2),collapse = ', '))),sep = '\n')
   response <- cbind(cc,peak.value=values,buffer)
   colnames(response) <- c('log(PI-H)','log(DAPI-H)','peak','buffer_0.25u')
@@ -85,6 +66,5 @@ findPeaks <- function(file,silent=F,pimp_my_plot=FALSE,peak_max=6) {
 }
 
 
-findPeaks(FACSC20141216[[2]])
+findPeaks(FACSC20141216_files[[1]])
 
-findPeaks(FACSC20141216_files[[2]],pimp_my_plot = T)

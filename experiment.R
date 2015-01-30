5ccdfba385ea0e583ce9050bf1f55b83a7e32266
Mario Fajardo <mario.fajardopedraza@sydney.edu.au>
2015-01-30 16:55:51 +1100

    print plot

--- experiment.R
+++ experiment.R
@@ -6,14 +6,15 @@ require(Rwave)
 require(EBImage)
 FACSC20141216_files <- dir('.','.fcs',full.names = T)
 
-findPeaks1 <- function(file,tolerance=.995,peaks=5,plot=F) {
-  resol=250
+findPeaks1 <- function(file,tolerance=.995,peaks=5, resol=250,plot=F) {
+
   data <- read.FCS(file, transformation=FALSE,alter.names = T)
-  to_plot <- data.frame(x=data@exprs[,'PI.H'],y=data@exprs[,'DAPI.H'])
+  mask<-data@exprs[,'PI.H']/data@exprs[,'DAPI.H']>=0.8&data@exprs[,'PI.H']/data@exprs[,'DAPI.H']<=1.2
+  to_plot <- data.frame(x=data@exprs[,'PI.H'],y=data@exprs[,'DAPI.H'])[mask,]
 #   plot(log(to_plot$x),log(to_plot$y))
   kern<-kde2d(log(to_plot$x),log(to_plot$y),n = resol)
+
   mask<-kern$z*as.numeric(kern$z>quantile(kern$z,tolerance))
-  
   
   to_plot_kern<-data.frame(x=rep(kern$x,resol),y=rep(kern$y,each=resol),z=melt(kern$z)[,3])
   
@@ -24,7 +25,7 @@ findPeaks1 <- function(file,tolerance=.995,peaks=5,plot=F) {
   loc_max_posit_x<-loc_max(rowSums(kern$z*mask))
   local_maximas_x<-kern$x[loc_max_posit_x]
   if(length(na.exclude(local_maximas_x))<peaks) {
-    warning(paste('only',length(na.exclude(local_maximas_x)),' peaks were higher that the tolerance value'))
+    warning(immediate. = T,call.=FALSE,paste('only',length(na.exclude(local_maximas_x)),' peaks were higher that the tolerance value'))
     local_maximas_x<-local_maximas_x[complete.cases(local_maximas_x)]
     peaks<-length(local_maximas_x)
   }
@@ -41,19 +42,24 @@ findPeaks1 <- function(file,tolerance=.995,peaks=5,plot=F) {
 #   coord_equal()+
 #   labs(x='log(PI-H)',y='log(DAPI-H)')
 if(plot){
-    ggplot(to_plot_kern,aes(x,y))+
+    print(ggplot(to_plot_kern,aes(x,y))+
     geom_tile(aes(fill=z))+
     coord_equal()+
     labs(x='log(PI-H)',y='log(DAPI-H)')+
     geom_vline(xintercept=local_maximas_x,linetype=2,colour=local_maximas_x)+
-    geom_hline(yintercept=local_maximas_y,linetype=2,colour=local_maximas_x)
+    geom_hline(yintercept=local_maximas_y,linetype=2,colour=local_maximas_x))
 }
 # pix_size_x <- abs(kern$x[1]- kern$x[2])
 # pix_size_y <- abs(kern$y[1]- kern$y[2])
 # 
 # 
 # buffer <- mapply(FUN = function(x,y) kern$z[x-x*(buffer_size/pix_size_x):x-buffer_size/pix_size_x,y],loc_max_posit_x,loc_max_posit_y)
+results <- round(data.frame(x_value=local_maximas_x,y_value=local_maximas_y),3)
+colnames(results) <- c('log(PI-H)','log(DAPI-H)')
 
+cat(paste0('Peaks for sample: \n##', basename(file), '##'),'\n')
+print(results)
+cat('#################################')
 }  
 
-findPeaks1(file = FACSC20141216_files[[3]],tolerance = .995,peaks = 5,plot = T)
+findPeaks1(file = FACSC20141216_files[[6]],tolerance = .995,resol = 200,peaks = 3,plot = T)
